147236451b3e037ab51342505a8f2996fc36de80
ruguoxiansheng <by_wwj@163.com>
2018-04-08 08:41:08 +0800

    源码测试。

---
+++ src/main/LinkedBlockingQueueTestPackage/LBQ.java
@@ -0,0 +1,218 @@
+package main.LinkedBlockingQueueTestPackage;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LBQ<E> extends LinkedBlockingQueue<E>  {
+
+    /**
+     * Linked list node class
+     */
+    static class Node<E> {
+        E item;
+
+        /**
+         * One of:
+         * - the real successor Node
+         * - this Node, meaning the successor is head.next
+         * - null, meaning there is no successor (this is the last node)
+         */
+        LBQ.Node<E> next;
+
+        Node(E x) { item = x; }
+    }
+
+    /** The capacity bound, or Integer.MAX_VALUE if none */
+    private final int capacity;
+
+    /** Current number of elements */
+    private final AtomicInteger count = new AtomicInteger();
+
+    /**
+     * Head of linked list.
+     * Invariant: head.item == null
+     */
+    transient LBQ.Node<E> head;
+
+    /**
+     * Tail of linked list.
+     * Invariant: last.next == null
+     */
+    private transient LBQ.Node<E> last;
+
+    /** Lock held by take, poll, etc */
+    private final ReentrantLock takeLock = new ReentrantLock();
+
+    /** Wait queue for waiting takes */
+    private final Condition notEmpty = takeLock.newCondition();
+
+    /** Lock held by put, offer, etc */
+    private final ReentrantLock putLock = new ReentrantLock();
+
+    /** Wait queue for waiting puts */
+    private final Condition notFull = putLock.newCondition();
+
+    /**
+     * Creates a {@code LinkedBlockingQueue} with a capacity of
+     * {@link Integer#MAX_VALUE}.
+     */
+    public LBQ() {
+        this(2);
+    }
+
+    /**
+     * Creates a {@code LinkedBlockingQueue} with the given (fixed) capacity.
+     *
+     * @param capacity the capacity of this queue
+     * @throws IllegalArgumentException if {@code capacity} is not greater
+     *         than zero
+     */
+    public LBQ(int capacity) {
+        if (capacity <= 0) throw new IllegalArgumentException();
+        this.capacity = capacity;
+        last = head = new Node<E>(null);
+    }
+
+    /**
+     * Inserts the specified element at the tail of this queue, waiting if
+     * necessary for space to become available.
+     *
+     * @throws InterruptedException {@inheritDoc}
+     * @throws NullPointerException {@inheritDoc}
+     */
+    public void put(E e) throws InterruptedException {
+        if (e == null) throw new NullPointerException();
+        // Note: convention in all put/take/etc is to preset local var
+        // holding count negative to indicate failure unless set.
+        int c = -1;
+        Node<E> node = new Node<E>(e);
+        final ReentrantLock putLock = this.putLock;
+        final AtomicInteger count = this.count;
+        System.out.println("SQ:threadname="+Thread.currentThread().getName());
+        putLock.lockInterruptibly();
+        try {
+            /*
+             * Note that count is used in wait guard even though it is
+             * not protected by lock. This works because count can
+             * only decrease at this point (all other puts are shut
+             * out by lock), and we (or some other waiting put) are
+             * signalled if it ever changes from capacity. Similarly
+             * for all other uses of count in other wait guards.
+             */
+            while (count.get() == capacity) {
+                System.out.println("CQ:threadname="+Thread.currentThread().getName());
+                Thread.sleep(3000l);
+                notFull.await();
+                System.out.println("AfterWait:threadname="+Thread.currentThread().getName());
+            }
+            enqueue(node);
+            c = count.getAndIncrement();
+            if (c + 1 < capacity)
+                notFull.signal();
+        } finally {
+            putLock.unlock();
+        }
+        if (c == 0)
+            signalNotEmpty();
+    }
+
+    /**
+     * Links node at end of queue.
+     *
+     * @param node the node
+     */
+    private void enqueue(Node<E> node) {
+        // assert putLock.isHeldByCurrentThread();
+        // assert last.next == null;
+        last = last.next = node;
+    }
+
+    /**
+     * Signals a waiting take. Called only from put/offer (which do not
+     * otherwise ordinarily lock takeLock.)
+     */
+    private void signalNotEmpty() {
+        final ReentrantLock takeLock = this.takeLock;
+        takeLock.lock();
+        try {
+            notEmpty.signal();
+        } finally {
+            takeLock.unlock();
+        }
+    }
+
+    public boolean remove(Object o) {
+        if (o == null) return false;
+        fullyLock();
+        try {
+            for (Node<E> trail = head, p = trail.next;
+                 p != null;
+                 trail = p, p = p.next) {
+                if (o.equals(p.item)) {
+                    unlink(p, trail);
+                    return true;
+                }
+            }
+            return false;
+        } finally {
+            fullyUnlock();
+        }
+    }
+
+    /**
+     * Locks to prevent both puts and takes.
+     */
+    void fullyLock() {
+        putLock.lock();
+        takeLock.lock();
+    }
+
+    /**
+     * Unlocks to allow both puts and takes.
+     */
+    void fullyUnlock() {
+        takeLock.unlock();
+        putLock.unlock();
+    }
+
+    /**
+     * Unlinks interior Node p with predecessor trail.
+     */
+    void unlink(Node<E> p, Node<E> trail) {
+        // assert isFullyLocked();
+        // p.next is not changed, to allow iterators that are
+        // traversing p to maintain their weak-consistency guarantee.
+        p.item = null;
+        trail.next = p.next;
+        if (last == p)
+            last = trail;
+        if (count.getAndDecrement() == capacity)
+            notFull.signal();
+    }
+
+    public String toString() {
+        fullyLock();
+        try {
+            Node<E> p = head.next;
+            if (p == null)
+                return "[]";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append('[');
+            for (;;) {
+                E e = p.item;
+                sb.append(e == this ? "(this Collection)" : e);
+                p = p.next;
+                if (p == null)
+                    return sb.append(']').toString();
+                sb.append(',').append(' ');
+            }
+        } finally {
+            fullyUnlock();
+        }
+    }
+
+
+}
--- src/main/LinkedBlockingQueueTestPackage/LBQTest5.java
+++ src/main/LinkedBlockingQueueTestPackage/LBQTest5.java
@@ -1,23 +1,19 @@
 package main.LinkedBlockingQueueTestPackage;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by Administrator on 2018/3/26.
- *示例目标：测试put中锁的行走机制
- * 调试方式：
- *  先将断点断在lbq.put("c");处，等个3秒中然后再走。
- *
  */
-public class LBQTest1 {
+public class LBQTest5 {
 
     public  static  void main(String[] args) {
-        LinkedBlockingQueue<String> lbq = new LinkedBlockingQueue<String>(2);
+       LBQ<String> lbq = new LBQ<String>();
         try {
             lbq.put("a");
             lbq.put("b");
+            System.out.println(lbq.toString());
+            // 至此队列已经被塞满了
              Thread thread1 = new Thread() {
                 public void run() {
                     try {
@@ -29,7 +25,8 @@ public class LBQTest1 {
             };
              thread1.setName("thread-1");
              thread1.start();
-            Thread.sleep(3000l);
+
+            Thread.sleep(1000l);
             Thread thread2 = new Thread() {
                 public void run() {
                     try {
@@ -41,21 +38,28 @@ public class LBQTest1 {
             };
             thread2.setName("thread-2");
             thread2.start();
+            Thread.sleep(1000);
+            Thread thread3 = new Thread() {
+                public void run() {
+                    try {
+                        lbq.put("e");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            thread3.setName("thread-3");
+            thread3.start();
 
-//            Thread thread3 = new Thread() {
-//                public void run() {
-//                    try {
-//                        lbq.put("e");
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            };
-//            thread3.setName("thread-3");
-//            thread3.start();
-////            lbq.put("c");
-//            Thread.sleep(3000l);
-//            lbq.put("e");
+            Thread.sleep(3000);
+            lbq.remove("a");
+
+
+            Thread.sleep(1000);
+            lbq.remove("b");
+            Thread.sleep(1000);
+
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
